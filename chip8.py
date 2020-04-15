faafead4d83bbd630f8b7b3d626f1add1c21e4df
unknown <utkarshr2811@gmail.com>
2020-04-15 15:43:36 +0530

    defined register, mapped keys and loaded ROMs

--- chip8.py
+++ chip8.py
@@ -78,3 +78,70 @@ pygame.display.set_caption("CHIP8 Emulator")
 pxarray = pygame.PixelArray(app_screen)
 clock = pygame.time.Clock()
 pygame.key.set_repeat(2000, 2000)
+
+
+
+
+
+
+REGISTERS_NUM = 0x10
+
+
+
+CLS_BG = (128, 128, 128)
+COL_FG = (160, 255, 160)
+
+CLS_BG = (100, 200, 100)
+COL_FG = (64, 64, 64)
+
+KEY_MAP = {
+    pygame.K_KP0: 0x0,
+    pygame.K_KP1: 0x1,
+    pygame.K_KP2: 0x2,
+    pygame.K_KP3: 0x3,
+    pygame.K_KP4: 0x4,
+    pygame.K_KP5: 0x5,
+    pygame.K_KP6: 0x6,
+    pygame.K_KP7: 0x7,
+    pygame.K_KP8: 0x8,
+    pygame.K_KP9: 0x9,
+    pygame.K_a: 0xA,
+    pygame.K_b: 0xB,
+    pygame.K_c: 0xC,
+    pygame.K_d: 0xD,
+    pygame.K_e: 0xE,
+    pygame.K_f: 0xF
+}
+
+KEY_MAP = {
+    pygame.K_KP0: 0x0,
+    pygame.K_KP7: 0x1,
+    pygame.K_KP8: 0x2,
+    pygame.K_KP9: 0x3,
+    pygame.K_KP4: 0x4,
+    pygame.K_KP5: 0x5,
+    pygame.K_KP6: 0x6,
+    pygame.K_KP1: 0x7,
+    pygame.K_KP2: 0x8,
+    pygame.K_KP3: 0x9,
+    pygame.K_a: 0xA,
+    pygame.K_b: 0xB,
+    pygame.K_c: 0xC,
+    pygame.K_d: 0xD,
+    pygame.K_e: 0xE,
+    pygame.K_f: 0xF
+}
+
+key_down = 0
+
+
+ROMs = collections.OrderedDict([
+    ("ROM/Pong2.ch8", 500),
+    ("ROM/SpaceInvaders.ch8", 500) ])
+
+
+ROM_index = 0
+ROM_filename = list(ROMs.keys())[ROM_index]
+ROM_FPS = list(ROMs.values())[ROM_index]
+
+FPS = ROM_FPS
