55e2631fae616428df707054f0ef4f566ba6c96d
unknown <utkarshr2811@gmail.com>
2020-04-15 15:10:13 +0530

    emulator display

--- chip8.py
+++ chip8.py
@@ -5,8 +5,75 @@ from time import time, sleep
 from pprint import pprint
 import collections
 
-
-import pygame.locals
-import colorama             
+from pygame.constants import K_SPACE, K_RETURN
+import colorama
 
 colorama.init()
+
+import winsound
+Freq = 3500
+Dur = 20
+
+import sysprint("Native byteorder: ", sys.byteorder)
+
+import logging
+logging.basicConfig(level-logging.DEBUG, format='%(levelname)s - %(message)s')
+
+logger = logging.getLogger()
+
+
+logger.setLevel(logging.CRITICAL)
+logger.setLevel(logging.WARN)
+
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+
+pygame.init()
+
+
+PYGAME_DISPLAY = True
+TEST_VRAM = False
+CONSOLE_CLS = False
+CONSOLE_DEBUG_SCREEN = False
+CONSOLE_DEBUG_MSG = False
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+PI = 3.1411592653
+
+display_width, display_height = 64, 32
+device_screen_in_pixels = display_width, display_height
+
+
+
+if PYGAME_DISPLAY:
+    screen_scale = 9
+else:
+    screen_scale = 1
+
+
+
+screen_status_h = 96
+
+app_display_width = display_width * screen_scale
+app_display_height = display_height * screen_scale + screen_status_h
+app+display_pixels_count = app_display_width * app_display_height
+
+app_display_size = (app_display_width, app_display_height)
+app_screen = pygame.display.set_mode(app_display_size)
+
+device_screen_pixels_count = display_width * screen_scale * display_height * screen_scale
+
+
+
+pygame.display.set_caption("CHIP8 Emulator")
+
+pxarray = pygame.PixelArray(app_screen)
+clock = pygame.time.Clock()
+pygame.key.set_repeat(2000, 2000)
