226fab1bb9ba233a3ec9f7a5334ab2f3db5f4af3
unknown <utkarshr2811@gmail.com>
2020-04-15 13:22:39 +0530

    imported libraries

---
+++ chip8.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+from random import randint, seed, randrange
+from time import time, sleep
+from pprint import pprint
+import collections
+
+
+from pygame.locals import *
+import colorama             
+
+colorama.init()
